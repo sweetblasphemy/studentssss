973e7e182a69707a4307b040759150dee08b11fb
sweetblasphemy <nik2006@abv.bg>
2023-05-13 10:57:44 +0300

    Add project files.

---
+++ studentssss/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace studentssss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student s1 = new student ("Niko", 15, 4492347, 4, 6, 5, 6, 5);
+            student s2 = new student("Finn", 15, 4207549, 5, 6, 2, 4, 6);
+            student s3 = new student("Ru", 15, 4874294, 5, 5, 6, 4, 6);
+            student s4 = new student("Luna", 15, 4871873, 5, 4, 3, 6, 6);
+
+            s1.print();
+            s2 .print();
+            s3 .print();
+            s4 .print();
+
+            av_result.print();
+
+            
+        }
+    }
+}
---
+++ studentssss/student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentssss
+{
+    internal class student
+    {
+
+        public string name;
+        public int age;
+        public int id;
+        public int grade_math;
+        public int grade_eng;
+        public int grade_lit;
+        public int grade_IT;
+        public int grade_bio;
+
+        public student(string name, int age, int id, int grade_math, int grade_eng, int grade_lit, int grade_IT, int grade_bio)
+        {
+
+            this.name = name;
+            this.age = age;
+            this.id = id;
+            this.grade_math = grade_math;
+            this.grade_eng = grade_eng;
+            this.grade_lit = grade_lit;
+            this.grade_IT = grade_IT;
+            this.grade_bio = grade_bio;
+
+            
+
+        }
+        public void print ()
+        {
+
+            Console.WriteLine ("The student's name: " + name + ", age: " + age  + ", ID: " + id + ", math grade: " + grade_math + ", english grade: " + grade_eng + ", literature grade: " + grade_lit + ", IT grade: " + grade_IT + ", biology grade: " + grade_bio);
+
+        }
+        public void print1()
+        {
+            int av_result = (grade_math + grade_eng + grade_lit + grade_IT + grade_bio) / 5;
+            Console.WriteLine (av_result);
+        }
+        public void print_maxGrade()
+        {
+
+            if (grade_math + grade_eng + grade_lit + grade_IT + grade_bio >> grade_math1 + grade_eng1 + grade_lit1 + grade_IT1 + grade_bio1)
+            {
+                Console.WriteLine("The student with the highest grade is: " + name);
+            }
+
+        }
+
+    }
+}
